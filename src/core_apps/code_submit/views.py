f65197e8774d78823f899d1232de73b64ae4e020
Mahboob-A <iammahboob.a@gmail.com>
2024-05-08 10:06:21 +0530

    refactor: CodeSubmit API has been refactored.
    The logging messages has been shifted to its respected process from the API class.

--- src/core_apps/code_submit/views.py
+++ src/core_apps/code_submit/views.py
@@ -19,7 +19,7 @@ import boto3
 # local
 from core_apps.code_submit.jwt_decode import jwt_decoder
 from core_apps.code_submit.process_data import data_processor
-from core_apps.code_submit.s3_handler import s3_data_uploader
+from core_apps.code_submit.s3_handler import s3_data_handler
 from core_apps.code_submit.mq_producer import mq_publisher
 
 
@@ -65,6 +65,24 @@ class SubmitCode(APIView):
                 status=status.HTTP_500_INTERNAL_SERVER_ERROR,
             )
 
+    def create_unique_object_key(self, data, lang):
+        """Create an Unique filename for S3 Upload"""
+        now = int(time.time())
+        username = data["user"].get("username")
+        file_prefix = f"{username}-{now}-{lang}"
+        object_key = f"codes/{file_prefix}-data.txt"
+        return object_key
+
+    def s3_upload(self, data, object_key):
+        """Upload to S3"""
+        data = json.dumps(data)
+        bytes_obj_data = io.BytesIO(data.encode())
+        # Upload to S3
+        s3_file_url, message = s3_data_handler.upload_file_to_s3(
+            object_key=object_key, bytes_obj_data=bytes_obj_data
+        )
+        return s3_file_url, message
+
     def post(self, request, format=None):
         """Decode JWT, get user details. Get Testcases from DB. Upload data to S3. Push FIle Link to MQ"""
         problem_id = request.data.get("problem_id")
@@ -77,33 +95,15 @@ class SubmitCode(APIView):
         if data is not None:
             submission_id = data.get("submission_id")
 
-            # create unique file name
-            now = int(time.time())
-            username = data["user"].get("username")
+            object_key = self.create_unique_object_key(data=data, lang=lang)
+            s3_file_url, message = self.s3_upload(data=data, object_key=object_key)
 
-            # convert data to string to convert it into bytes
-            data = json.dumps(data)
-            bytes_obj_data = io.BytesIO(data.encode())
-
-            file_prefix = f"{username}-{now}-{lang}"
-            object_key = f"codes/{file_prefix}-data.txt"  # object_key is the filename
-
-            # Upload to S3
-            file_url, message = s3_data_uploader.upload_file_to_s3(
-                object_key=object_key, bytes_obj_data=bytes_obj_data
-            )
-
-            # file_url == s3 upload success
-            if file_url is not None:
-                logger.info(
-                    f"\n[SUCCESS]: The User Code Files with Object Key: '{object_key}' Successfully Uploaded to S3."
+            # s3_file_url == s3 upload success. publish to mq
+            if s3_file_url is not None:
+                published, message = mq_publisher.publish_data(
+                    uploaded_s3_data_link=s3_file_url, object_key=object_key
                 )
-                try:
-                    # publish the link to mq
-                    mq_publisher.publish_data(uploaded_s3_data_link=file_url)
-                    logger.info(
-                        f"\n[SUCCESS]: The User Code Files with Object Key: '{object_key}' Successfully Published to MQ."
-                    )
+                if published:
                     return Response(
                         {
                             "result": {
@@ -113,18 +113,13 @@ class SubmitCode(APIView):
                         },
                         status=status.HTTP_200_OK,
                     )
-                except Exception as e:
-                    logger.exception(
-                        f"\n[X]: The User Code Files with Object Key: '{object_key}' Could not be published to MQ.\n[EXCEPTION]: {str(e)}"
-                    )
-                    s3_data_uploader.delete_uploaded_file(object_key)
-                    message = "error-publishing-to-mq"
+                else:
+                    # s3 data link publish to MQ failed. delete s3 data link from s3 bucket
+                    s3_data_handler.delete_uploaded_file(object_key)
                     return self.process_error_response(message=message)
-            else:
-                logger.error(
-                    f"\n[X]: The Object Key: {object_key} could not be Uploaded to S3."
-                )
+            else: 
+                # data upload to s3 failed.
                 return self.process_error_response(message=message)
-        else:
-            logger.error(f"\n[X]: Data could not be processed. The error is: {message}")
+        else: 
+            # jwt and fetch question details in data process failed.
             return self.process_error_response(message=message, problem_id=problem_id)
