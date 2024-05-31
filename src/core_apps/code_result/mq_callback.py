2010f49e0e3b6df3ede420802f6ed8099807ad8e
Mahboob-A <iammahboob.a@gmail.com>
2024-05-31 18:32:46 +0530

    integrate  mongodb: integrate mongodb for code execution result persistence.
    support for mongodb is added. the code execution results are now persisted in mongodb. the result is cached once consumed from RCE Engine service, and then saved to mongodb database.

--- src/core_apps/code_result/mq_callback.py
+++ src/core_apps/code_result/mq_callback.py
@@ -3,8 +3,13 @@ import logging
 import time
 import traceback
 
+
 import redis
 from django.core.exceptions import ImproperlyConfigured
+from django.conf import settings
+
+# mongodb
+from db_connection import mongo_result_collection
 
 # # MQ Code Execution Result Consume
 from core_apps.code_result.code_exec_result_consumer import result_consumer_mq
@@ -13,7 +18,7 @@ logger = logging.getLogger(__name__)
 
 
 try:
-    r = redis.Redis(host="code-manager-redis", port=6379, db=0)
+    redis_client = redis.Redis(host="code-manager-redis", port=6379, db=0)
 except (ImproperlyConfigured, Exception) as e:
     logger.exception(
         f"\n[Redis Import Error]: Error Occurred During Importing Reids\n[EXCEPTION]: {str(e)}"
@@ -34,21 +39,24 @@ def callback(channel, method, properties, body):
     try:
         # body is in bytes. decodes to str then as dict
         result_data = json.loads(body.decode("utf-8"))
-        print("\n\nResult Data In Code Manager: ", result_data)
 
-        # Save the result in cache  for immediate polling
+        # TODO  SLEEP is only for demonstrating the Long Polling Only. Remove the SLEEP.
+        # time.sleep(50)
+
+        # cached for immediate polling
         submission_id = result_data.get("submission_id")
-        r.set(submission_id, json.dumps(result_data))
-        r.expire(name=submission_id, time=60)
+        redis_client.set(submission_id, json.dumps(result_data))  #save as json 
+        redis_client.expire(
+            name=submission_id, time=settings.REDIS_CACHE_TIME_IN_SECONDS
+        )  # currently 15 seconds
 
-        # save the result in db for persistence.
+        # saved in mongodb for persistence.
+        mongo_result_collection.insert_one(result_data)
 
         username = result_data["user_details"].get("username")
-
         logger.info(
             f"[Code Result Consume Success]: Code Result Consume Successful for Username: {username}"
         )
-
     except Exception as e:
         logger.exception(
             f"[MQ Callback EXCEPTION]: Exception Occurred at Callback.\n[EXCEPTION]: {str(e)}"
--- src/docker/dev/postgres/init-user-db.sh
+++ src/docker/dev/postgres/init-user-db.sh
@@ -3,7 +3,7 @@ set -e
 
 # 290524, Wednesday 
 # this script is being used to create a custom user and db name using the default superuser postgres and custom db postgres
-# it has come conditions, the data should be empty to run the script. 
+# the below script has some conditions, the data should be empty to run the script. 
 # see more on this link at: [Initialization scripts] section. Link: https://hub.docker.com/_/postgres?tab=description
 
 psql -v ON_ERROR_STOP=1 --username postgres --dbname postgres <<-EOSQL
